1198b49e9f9d553a5347e51fe0df815a06406420
Ciao-Wen-Chen <ogh478@gmail.com>
2022-02-22 00:20:27 +0000

    Chapter 10 completed

--- rango/views.py
+++ rango/views.py
@@ -15,6 +15,9 @@ from rango.models import Page
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
+#@ Chapter 10
+from datetime import datetime
+
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Chapter 6
@@ -31,8 +34,15 @@ def index(request):
     
     #@ Chapter 3
     #return HttpResponse("Rango says hey there partner!<br> <a href='/rango/about'>About</a>.")
+
+    #@ Chapter 10
+    visitor_cookie_handler(request)
+    #context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
+
     #@ Chapter 4 
-    return render(request, 'rango/index.html', context=context_dict)
+    #return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     #@ Chapter 3
@@ -42,7 +52,16 @@ def about(request):
     #@ Chapter 8 
     print(request.method)
     print(request.user)
-    return render(request, 'rango/about.html', {})
+
+    #@ Chapter 10
+    context_dict = {}
+    if request.session.test_cookie_worked(): 
+        print("TEST COOKIE WORKED!") 
+        request.session.delete_test_cookie()
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    return render(request, 'rango/about.html', context=context_dict)
     
 
 #Chapter 6
@@ -216,3 +235,35 @@ def user_logout(request):
     logout(request)
     # Take the user back to the homepage.
     return redirect(reverse('rango:index'))
+
+#@ Chapter 10
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None): 
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val 
+    return val
+
+# Updated the function definition
+def visitor_cookie_handler(request):
+
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer. 
+    # If the cookie doesn't exist, then the default value of 1 is used. 
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count 
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie 
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits'] = visits
